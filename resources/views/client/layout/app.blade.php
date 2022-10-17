d0e0a4cac7e27a5612591fa2189a5e352217da16
Hatim Lagzar <lhatimdev@gmail.com>
2022-10-17 16:57:16 +0100

    add flags for languages

--- resources/views/client/layout/app.blade.php
+++ resources/views/client/layout/app.blade.php
@@ -65,12 +65,14 @@
       <div class="languages-selector-wrapper ms-auto">
         <div class="dropdown">
           <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            English
+            <img width="20" src="/images/flags-languages/en.svg" alt="English"> English
           </button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Arabic</a></li>
-            <li><a class="dropdown-item" href="#">Francais</a></li>
-            <li><a class="dropdown-item" href="#">Deutsch</a></li>
+          <ul class="dropdown-menu" aria-labelledby="flags-dropdown" id="flags-dowpdown-list">
+            <li><a class="dropdown-item" href="#"><img src="/images/flags-languages/en.svg" alt="English">English</a></li>
+            <li><a class="dropdown-item" href="#"><img src="/images/flags-languages/fr.svg" alt="French">French</a></li>
+            <li><a class="dropdown-item" href="#"><img src="/images/flags-languages/es.svg" alt="Spanish">Spanish</a></li>
+            <li><a class="dropdown-item" href="#"><img src="/images/flags-languages/it.svg" alt="Italian">Italian</a></li>
+            <li><a class="dropdown-item" href="#"><img src="/images/flags-languages/de.svg" alt="Deutsch">Deutsch</a></li>
           </ul>
         </div>
       </div>
