e44d07ec39cad394ea2774c9513e9aa2ce902970
Hatim Lagzar <lhatimdev@gmail.com>
2022-10-29 18:12:51 +0100

    remove unecessary code

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -8,13 +8,6 @@ use Stevebauman\Location\Facades\Location;
 
 class HomeController extends Controller
 {
-    private CountryService $countryService;
-
-    public function __construct(CountryService $countryService)
-    {
-        $this->countryService = $countryService;
-    }
-
     public function __invoke()
     {
         $countryCode = null;
