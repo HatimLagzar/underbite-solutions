90811e7e76a865ccd32e417807b67ba9a409b6e3
Hatim Lagzar <lhatimdev@gmail.com>
2022-10-29 18:14:44 +0100

    add try catch

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,18 +3,29 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Stevebauman\Location\Facades\Location;
+use Throwable;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        $countryCode = null;
-        if (Location::get(request()->ip())) {
-            $countryCode = Location::get(request()->ip())->countryCode;
-        }
+        try {
+            $countryCode = null;
+            if (Location::get(request()->ip())) {
+                $countryCode = Location::get(request()->ip())->countryCode;
+            }
 
-        return view('client.home')
-            ->with('countryCode', $countryCode);
+            return view('client.home')
+                ->with('countryCode', $countryCode);
+
+        } catch (Throwable $e) {
+            Log::error('failed to show home page', [
+                'error_message' => $e->getMessage(),
+            ]);
+
+            abort(500);
+        }
     }
 }
