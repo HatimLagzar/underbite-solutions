4c36274fe584fef2e21cbfbfe60ca1e4ba88c4a7
Hatim Lagzar <lhatimdev@gmail.com>
2022-11-28 12:01:00 +0100

    reorder if statements

--- public/js/pages/home.js
+++ public/js/pages/home.js
@@ -80,14 +80,14 @@ form.querySelectorAll('input[type=file]').forEach(function (inputElement) {
 var webcamElement = document.getElementById('webcam-live');
 var canvasElement = document.getElementById('picture-canvas');
 webcamElement.width = 600;
+if (window.innerWidth <= 800) {
+  webcamElement.width = 500;
+}
 if (window.innerWidth <= 425) {
   webcamElement.width = 375;
 }
 if (window.innerWidth <= 375) {
   webcamElement.width = 300;
-}
-if (window.innerWidth <= 800) {
-  webcamElement.width = 500;
 }
 var webcam = new Webcam["default"](webcamElement, 'user', canvasElement);
 var selectedInputId = null;
