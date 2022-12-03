58a411cec0a25d404ab34c2f489617bb04ac3ccd
Hatim Lagzar <lhatimdev@gmail.com>
2022-12-03 21:33:57 +0100

    remove all option

--- resources/views/components/advanced-patient-search.blade.php
+++ resources/views/components/advanced-patient-search.blade.php
@@ -4,7 +4,6 @@
             <div class="form-group d-inline-block">
                 <label for="age">Age</label>
                 <select name="age[]" id="age" class="selectpicker form-select form-select-sm w-auto" multiple>
-                    <option value>Select Age</option>
                     @foreach(\App\Models\Patient::AVAILABLE_AGES as $stringRange => $arrayHeight)
                         <option value="{{ $stringRange }}" {{ request('age') && in_array($stringRange, request('age')) ? 'selected' : '' }}>{{ $stringRange }}</option>
                     @endforeach
@@ -14,7 +13,6 @@
             <div class="form-group d-inline-block">
                 <label for="height">Height</label>
                 <select name="height[]" id="height" class="selectpicker" multiple>
-                    <option value>Select Height</option>
                     @foreach(\App\Models\Patient::AVAILABLE_HEIGHTS as $stringRange => $array)
                         <option value="{{ $stringRange }}" {{ request('height') && in_array($stringRange, request('height')) ? 'selected' : '' }}>{{ $stringRange }}</option>
                     @endforeach
@@ -24,7 +22,6 @@
             <div class="form-group d-inline-block">
                 <label for="weight">Weight</label>
                 <select name="weight[]" id="weight" class="selectpicker" multiple>
-                    <option value>Select Weight</option>
                     @foreach(\App\Models\Patient::AVAILABLE_WEIGHTS as $stringRange => $array)
                         <option value="{{ $stringRange }}" {{ request('weight') && in_array($stringRange, request('weight')) ? 'selected' : '' }}>{{ $stringRange }}</option>
                     @endforeach
@@ -34,7 +31,6 @@
             <div class="form-group d-inline-block">
                 <label for="country">Country</label>
                 <select name="country[]" id="country" class="selectpicker" multiple>
-                    <option value>All</option>
                     @foreach(\App\Models\Country::all() as $country)
                         <option
                           value="{{ $country->getCode() }}" {{ request('country') && in_array($country->getCode(), request('country')) ? 'selected' : '' }}>{{ $country->getName() }}</option>
@@ -44,7 +40,6 @@
             <div class="form-group d-inline-block">
                 <label for="continent">Continent</label>
                 <select name="continent[]" id="continent" class="selectpicker" multiple>
-                    <option value>All</option>
                     @foreach(\App\Models\Continent::all() as $continent)
                         <option
                           value="{{ $continent->getCode() }}" {{ request('continent') && in_array($continent->getCode(), request('continent')) ? 'selected' : '' }}>{{ $continent->getName() }}</option>
@@ -54,7 +49,6 @@
             <div class="form-group d-inline-block">
                 <label for="gender">Gender</label>
                 <select name="gender" id="gender" class="form-select form-select-sm w-auto" style="max-width: 100px;">
-                    <option value>All</option>
                     <option value="1" {{ request()->get('gender') === '1' ? 'selected' : '' }}>Male</option>
                     <option value="2" {{ request()->get('gender') === '2' ? 'selected' : '' }}>Female</option>
                 </select>
