5b2434903a5731eb5827b899a4a372fb6806f4aa
Hatim Lagzar <lhatimdev@gmail.com>
2023-01-01 19:49:30 +0100

    show selected langage

--- resources/views/components/flags-menu.blade.php
+++ resources/views/components/flags-menu.blade.php
@@ -1,6 +1,18 @@
 <div class="dropdown">
   <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <img width="20" src="/images/flags-languages/en.svg" alt="English"> English
+    @if(app()->getLocale() === 'fr')
+      <img width="20" src="/images/flags-languages/fr.svg" alt="French"> French
+    @elseif(app()->getLocale() === 'ar')
+      <img width="20" src="/images/flags-languages/ar.svg" alt="Arabic"> Arabic
+    @elseif(app()->getLocale() === 'es')
+      <img width="20" src="/images/flags-languages/es.svg" alt="Spanish"> Spanish
+    @elseif(app()->getLocale() === 'de')
+      <img width="20" src="/images/flags-languages/de.svg" alt="Deutsch"> Deutsch
+    @elseif(app()->getLocale() === 'it')
+      <img width="20" src="/images/flags-languages/it.svg" alt="Italian"> Italian
+    @else
+      <img width="20" src="/images/flags-languages/en.svg" alt="English"> English
+    @endif
   </button>
   <ul class="dropdown-menu" aria-labelledby="flags-dropdown" id="flags-dowpdown-list">
     <li>
